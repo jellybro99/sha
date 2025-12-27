db19fab8ed8873cd79ce053c12a0954273cdbf2e
jellybro99 <benluke1111@gmail.com>
2025-12-27 09:18:46 -0600

    preprocess message slop

--- sha256.go
+++ sha256.go
@@ -1,14 +1,41 @@
 package main
 
+import (
+	"encoding/binary"
+	"fmt"
+)
+
 func sha256Hash(message string) []uint32 {
 	paddedMessageBytes := preprocessMessage(message)
 	hashArray := preprocessHash()
+
+	fmt.Println(string(paddedMessageBytes))
 
 	return hashArray
 }
 
 func preprocessMessage(message string) []byte {
 	messageBytes := []byte(message)
+
+	messsageLength := len(messageBytes) * 8
+	paddingLength := 448 - (messsageLength+1)%512
+	paddingNum := (1 << paddingLength)
+	paddingBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(paddingBytes, uint64(paddingNum))
+
+	// paddingBytes2 := make([]byte, 8)
+	// binary.BigEndian.PutUint64(paddingBytes2, uint64(messsageLength))
+
+	messageBytes = append(messageBytes, paddingBytes...)
+	// messageBytes = append(messageBytes, paddingBytes2...)
+
+	fmt.Println(message)
+	fmt.Println(string(messageBytes))
+	fmt.Println(messsageLength)
+	fmt.Println(paddingLength)
+	fmt.Println(paddingNum)
+	fmt.Println(string(paddingBytes))
+	fmt.Println(len(messageBytes) * 8)
 	// append 1
 	// append k 0s
 	// append 64 bit representation of l

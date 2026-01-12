4015e79daedabdf15298eb2aa435de593f4d9995
jellybro99 <benluke1111@gmail.com>
2026-01-11 19:38:22 -0600

    removed else statement

--- cmd/hasher.go
+++ cmd/hasher.go
@@ -12,27 +12,27 @@ type Hasher func(string) [8]uint32
 func hasher(hashFunction Hasher, outputFormat string, messages []string) error {
 	if len(messages) == 1 {
 		fmt.Print(formatHash(hashFunction(messages[0]), outputFormat))
-	} else {
-		var wg sync.WaitGroup
-		jobs := make(chan string)
-
-		workers := runtime.GOMAXPROCS(0)
-		for range workers {
-			wg.Go(func() {
-				for message := range jobs {
-					fmt.Printf("%s: %s", message, formatHash(hashFunction(message), outputFormat))
-				}
-			})
-		}
-
-		for _, message := range messages {
-			jobs <- message
-		}
-		close(jobs)
-
-		wg.Wait()
+		return nil
 	}
 
+	var wg sync.WaitGroup
+	jobs := make(chan string)
+
+	workers := runtime.GOMAXPROCS(0)
+	for range workers {
+		wg.Go(func() {
+			for message := range jobs {
+				fmt.Printf("%s: %s", message, formatHash(hashFunction(message), outputFormat))
+			}
+		})
+	}
+
+	for _, message := range messages {
+		jobs <- message
+	}
+	close(jobs)
+
+	wg.Wait()
 	return nil
 }
 
